bfb1888aecab6bd455694bbbd87360b16ef700a8
ducviet5138 <ducviet5138@gmail.com>
2023-05-13 20:47:33 +0700

    Update Converter

--- Converter.py
+++ Converter.py
@@ -7,14 +7,13 @@ def Converter():
     fo = open("PrologPython.py", 'r')
 
     line = "Not Empty String"
-
+    removeDuplicate = 0
+    
     while (1):
         line = fi.readline()
 
         if (not line):
             break
-        
-        removeDuplicate = 0
 
         if (line.find(":-") != -1):
             if (removeDuplicate == 0):
@@ -23,8 +22,54 @@ def Converter():
                 fo.write("name = sorted(set(name))\n")
                 fo = open("PrologPython.py", 'r')
                 
-            print("Found :-")
-            break
+            ######### Test #########
+            fo = open("PrologPython.py", 'a')
+            FuncName = line.split('(')[0]
+            Parameter = list()
+
+            OpenPos = 0
+            while (1):
+                OpenPos = line.find('(', OpenPos)
+                if (OpenPos == -1):
+                    break
+                ClosePos = line.find(')', OpenPos)
+                key = line[OpenPos + 1 : ClosePos]
+
+                if (key.find(',') != -1):
+                    temp = key.split(',')[0].strip()
+                    if (temp not in Parameter): Parameter.append(temp)
+                    temp = key.split(',')[1].strip()
+                    if (temp not in Parameter): Parameter.append(temp)
+                else:
+                    temp = key.strip()
+                    if (temp not in Parameter): Parameter.append(temp)
+
+                OpenPos = ClosePos + 1
+
+            Condition = line.split(':-')[1].strip()
+            Condition = Condition.replace("), ", ") and ")
+            Condition = Condition.replace(".", "")
+            Condition = Condition.replace("\=", "!=")
+            Condition = Condition.replace("(", "f(")
+            for i in range(len(Parameter)):
+                Condition = Condition.replace(Parameter[i], "i" + str(i))
+            Condition = "if" + " (" + Condition + "):"
+
+            fo.write("def " + FuncName + "f(X, Y):\n")
+            fo.write("    res = list()\n")
+            for i in range(len(Parameter)):
+                fo.write("    " * (i + 1) + "for i" + str(i) + " in " + "name:\n")
+            fo.write("    " * (len(Parameter) + 1) + Condition + "\n")
+            fo.write("    " * (len(Parameter) + 2) + "if (X == 0 and Y == 0):  res.append([i0, i1])\n")
+            fo.write("    " * (len(Parameter) + 2) + "elif (X == 0 and Y == i1): res.append(i0)\n")
+            fo.write("    " * (len(Parameter) + 2) + "elif (X == i0 and Y == 0): res.append(i1)\n")
+            fo.write("    " * (len(Parameter) + 2) + "elif (X == i0 and Y == i1): return True\n")
+
+            fo.write("    if(len(res) == 0): return False\n")
+            fo.write("    res = list(set(res))\n")
+            fo.write("    return res\n")
+            fo = open("PrologPython.py", 'r')
+            ######### End Test #########
         elif (line != "\n"):
             ListName = line.split('(')[0]
             AllFile = fo.read()
